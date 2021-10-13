10f56f5815dc00e0718a1e2d1a1d2625bc069648
Dell <bartek007k@gmail.com>
2021-10-14 01:22:28 +0200

    Endpoints added. Tested manually. Everything works fine. Added error handling. Next step: automatic tests.

--- src/main/java/com/future/cinemaxx/configuration/DataSetup.java
+++ src/main/java/com/future/cinemaxx/configuration/DataSetup.java
@@ -6,10 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Configuration
-public class DataSetup implements CommandLineRunner {
+public class DataSetup  { //implements CommandLineRunner
     CategoryRepo categoryRepo;
     CinemaHallRepo cinemaHallRepo;
     GenreRepo genreRepo;
@@ -34,55 +35,56 @@ public class DataSetup implements CommandLineRunner {
         this.ticketRepo = ticketRepo;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Theater theater = theaterRepo.save(new Theater("Cinema city", "Copenhagen", "Husumtorv", 25));
-        Theater theater1 = theaterRepo.save(new Theater("Whatever", "Copenhagen", "Test",11));
-
-        CinemaHall cinemaHall1 = cinemaHallRepo.save(new CinemaHall("A", 12, 16, theater));
-        CinemaHall cinemaHall2 = cinemaHallRepo.save(new CinemaHall("B", 8, 12, theater));
-        CinemaHall cinemaHall3 = cinemaHallRepo.save(new CinemaHall("C", 10, 14, theater));
-        CinemaHall cinemaHall4 = cinemaHallRepo.save(new CinemaHall("A", 7, 21, theater1));
-        CinemaHall cinemaHall5 = cinemaHallRepo.save(new CinemaHall("B", 15, 4, theater1));
-        CinemaHall cinemaHall6 = cinemaHallRepo.save(new CinemaHall("C", 9, 14, theater1));
-
-        Category category1 = categoryRepo.save(new Category("A", 8));
-        Category category2 = categoryRepo.save(new Category("B", 12));
-        Category category3 = categoryRepo.save(new Category("C", 16));
-        Category category4 = categoryRepo.save(new Category("D", 18));
-
-        Genre genre1 = genreRepo.save(new Genre("Horror"));
-        Genre genre2 = genreRepo.save(new Genre("Comedy"));
-        Genre genre3 = genreRepo.save(new Genre("Drama"));
-        Genre genre4 = genreRepo.save(new Genre("Fantasy"));
-
-        Movie movie1 = movieRepo.save(new Movie("Scary movie",Duration.ofMinutes(125) , genre2, category4));
-        Movie movie2 = movieRepo.save(new Movie("Spider-man", Duration.ofMinutes(140), genre4, category1));
-        Movie movie3 = movieRepo.save(new Movie("Gladiator",Duration.ofMinutes(115) , genre1, category3));
-        Movie movie4 = movieRepo.save(new Movie("Last movie",Duration.ofMinutes(163) , genre3, category2));
-        Movie movie5 = movieRepo.save(new Movie("Inception",Duration.ofMinutes(200) , genre3, category3));
-        Movie movie6 = movieRepo.save(new Movie("Dark Knight",Duration.ofMinutes(220) , genre3, category3));
-        Movie movie7 = movieRepo.save(new Movie("Deadpool",Duration.ofMinutes(163) , genre2, category4));
-
-        Projection projection1 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall1, movie1));
-        Projection projection2 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(7), 15.6f, cinemaHall1, movie2));
-        Projection projection3 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall3, movie4));
-        Projection projection4 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall2, movie3));
-        Projection projection5 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(1).plusHours(2), 15.6f, cinemaHall4, movie2));
-        Projection projection6 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(100).plusHours(2), 15.6f, cinemaHall4, movie2));
-        Projection projection7 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(22).plusHours(12), 15.6f, cinemaHall4, movie2));
-        Projection projection8 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(22), 15.6f, cinemaHall1, movie4));
-        Projection projection9 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(11), 15.6f, cinemaHall2, movie4));
-        Projection projection10 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(5), 15.6f, cinemaHall3, movie4));
-        Projection projection11 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(5), 15.6f, cinemaHall3, movie4));
-        Projection projection12 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(9), 15.6f, cinemaHall5, movie5));
-        Projection projection13 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(12), 15.6f, cinemaHall5, movie5));
-        Projection projection14 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(13), 15.6f, cinemaHall5, movie5));
-        Projection projection15 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(1), 15.6f, cinemaHall5, movie6));
-        Projection projection16 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(1), 15.6f, cinemaHall5, movie6));
-        Projection projection17 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall4, movie7));
-        Projection projection18 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall5, movie7));
-        Projection projection19 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall6, movie7));
-        Projection projection20 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(3), 15.6f, cinemaHall1, movie7));
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Theater theater = theaterRepo.save(new Theater("Cinema city", "Copenhagen", "Husumtorv", 25));
+//        Theater theater1 = theaterRepo.save(new Theater("Whatever", "Copenhagen", "Test",11));
+//
+//        CinemaHall cinemaHall1 = cinemaHallRepo.save(new CinemaHall("A", 12, 16, theater));
+//        CinemaHall cinemaHall2 = cinemaHallRepo.save(new CinemaHall("B", 8, 12, theater));
+//        CinemaHall cinemaHall3 = cinemaHallRepo.save(new CinemaHall("C", 10, 14, theater));
+//        CinemaHall cinemaHall4 = cinemaHallRepo.save(new CinemaHall("A", 7, 21, theater1));
+//        CinemaHall cinemaHall5 = cinemaHallRepo.save(new CinemaHall("B", 15, 4, theater1));
+//        CinemaHall cinemaHall6 = cinemaHallRepo.save(new CinemaHall("C", 9, 14, theater1));
+//
+//        Category category1 = categoryRepo.save(new Category("A", 8));
+//        Category category2 = categoryRepo.save(new Category("B", 12));
+//        Category category3 = categoryRepo.save(new Category("C", 16));
+//        Category category4 = categoryRepo.save(new Category("D", 18));
+//
+//        Genre genre1 = genreRepo.save(new Genre("Horror"));
+//        Genre genre2 = genreRepo.save(new Genre("Comedy"));
+//        Genre genre3 = genreRepo.save(new Genre("Drama"));
+//        Genre genre4 = genreRepo.save(new Genre("Fantasy"));
+//
+//        Movie movie1 = movieRepo.save(new Movie("Scary movie",Duration.ofMinutes(125) , genre2, category4));
+//        Movie movie2 = movieRepo.save(new Movie("Spider-man", Duration.ofMinutes(140), genre4, category1));
+//        Movie movie3 = movieRepo.save(new Movie("Gladiator",Duration.ofMinutes(115) , genre1, category3));
+//        Movie movie4 = movieRepo.save(new Movie("Last movie",Duration.ofMinutes(163) , genre3, category2));
+//        Movie movie5 = movieRepo.save(new Movie("Inception",Duration.ofMinutes(200) , genre3, category3));
+//        Movie movie6 = movieRepo.save(new Movie("Dark Knight",Duration.ofMinutes(220) , genre3, category3));
+//        Movie movie7 = movieRepo.save(new Movie("Deadpool",Duration.ofMinutes(163) , genre2, category4));
+//
+//
+//        Projection projection1 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall1, movie1));
+//        Projection projection2 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(7), 15.6f, cinemaHall1, movie2));
+//        Projection projection3 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall3, movie4));
+//        Projection projection4 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall2, movie3));
+//        Projection projection5 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(1).plusHours(2), 15.6f, cinemaHall4, movie2));
+//        Projection projection6 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(100).plusHours(2), 15.6f, cinemaHall4, movie2));
+//        Projection projection7 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(22).plusHours(12), 15.6f, cinemaHall4, movie2));
+//        Projection projection8 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(22), 15.6f, cinemaHall1, movie4));
+//        Projection projection9 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(11), 15.6f, cinemaHall2, movie4));
+//        Projection projection10 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(5), 15.6f, cinemaHall3, movie4));
+//        Projection projection11 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(5), 15.6f, cinemaHall3, movie4));
+//        Projection projection12 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(9), 15.6f, cinemaHall5, movie5));
+//        Projection projection13 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(12), 15.6f, cinemaHall5, movie5));
+//        Projection projection14 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(13), 15.6f, cinemaHall5, movie5));
+//        Projection projection15 = projectionRepo.save(new Projection(LocalDateTime.parse("2025-11-12T12:30:00"), 15.6f, cinemaHall5, movie6));
+//        Projection projection16 = projectionRepo.save(new Projection(LocalDateTime.parse("2025-11-14T11:35:00"), 15.6f, cinemaHall5, movie6));
+//        Projection projection17 = projectionRepo.save(new Projection(LocalDateTime.parse("2025-11-14T09:35:00"), 15.6f, cinemaHall4, movie7));
+//        Projection projection18 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall5, movie7));
+//        Projection projection19 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(10), 15.6f, cinemaHall6, movie7));
+//        Projection projection20 = projectionRepo.save(new Projection(LocalDateTime.now().plusDays(3), 15.6f, cinemaHall1, movie7));
+//    }
 }
--- src/test/java/com/future/cinemaxx/controllers/projections/ProjectionControllerImplTest.java
+++ src/test/java/com/future/cinemaxx/controllers/projections/ProjectionControllerImplTest.java
@@ -1,60 +1,153 @@
 package com.future.cinemaxx.controllers.projections;
 
+import com.future.cinemaxx.dtos.ProjectionDTO;
+import com.future.cinemaxx.repositories.*;
+import com.future.cinemaxx.testUtils.TestDataMaker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.expression.spel.ast.Projection;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@AutoConfigureTestDatabase
+@EnableAutoConfiguration
+@SpringBootTest(classes = {com.future.cinemaxx.CinemaxxApplication.class},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProjectionControllerImplTest {
+
+    private final String BASE_PATH = "/api/projections";
+    private final HttpHeaders headers = new HttpHeaders();
+    @LocalServerPort
+    private int port;
+    TestRestTemplate restTemplate;
+
+    //Used to set up mock data for testing
+
+    CategoryRepo categoryRepo;
+    CinemaHallRepo cinemaHallRepo;
+    MovieRepo movieRepo;
+    ProjectionRepo projectionRepo;
+    TheaterRepo theaterRepo;
+    GenreRepo genreRepo;
+
+    //Used for storing the ids of theaters and projections no matter what are they generated as
+
+    ArrayList<Integer>[] ids;
+
+    @Autowired
+    public ProjectionControllerImplTest(CategoryRepo categoryRepo, CinemaHallRepo cinemaHallRepo, MovieRepo movieRepo,
+                                        ProjectionRepo projectionRepo, TheaterRepo theaterRepo, GenreRepo genreRepo,
+                                        TestRestTemplate restTemplate) {
+        this.categoryRepo = categoryRepo;
+        this.cinemaHallRepo = cinemaHallRepo;
+        this.movieRepo = movieRepo;
+        this.projectionRepo = projectionRepo;
+        this.theaterRepo = theaterRepo;
+        this.genreRepo = genreRepo;
+        this.restTemplate = restTemplate;
+    }
+
+
+    @BeforeEach
+    public void setUpData(){
+        ids= TestDataMaker.makeDataForTests(theaterRepo,cinemaHallRepo,categoryRepo,genreRepo,movieRepo,projectionRepo);
+    }
+
 
     @Test
     void getAll() {
+        ResponseEntity<List<ProjectionDTO>> response = getResponseFromAllProjections();
+        assertEquals(20,response.getBody().size());
+        assertEquals(response.getBody().get(0).getMovie().getName(), "Scary movie");
     }
 
     @Test
     void getById() {
+        HttpEntity<String> entity = new HttpEntity<>(null,headers);
+        ResponseEntity<ProjectionDTO> response = restTemplate.exchange(makeUrl(BASE_PATH+ "/"+ids[1].get(3)),
+                HttpMethod.GET,
+                entity,
+                ProjectionDTO.class);
+        assertEquals("B",response.getBody().getCinemaHallName());
+        assertEquals("Gladiator",response.getBody().getMovie().getName());
     }
 
     @Test
     void getByTheaterId() {
+        HttpEntity<String> entity = new HttpEntity<>(null,headers);
+        ResponseEntity<List<ProjectionDTO>> response = restTemplate.exchange(makeUrl(BASE_PATH+"/theater/"+ids[0].get(0)),
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<ProjectionDTO>>(){});
+        assertEquals("Scary movie",response.getBody().get(0).getMovie().getName());
+        assertEquals("Spider-man",response.getBody().get(1).getMovie().getName());
+        assertEquals("Last movie",response.getBody().get(5).getMovie().getName());
+        assertEquals("B",response.getBody().get(5).getCinemaHallName());
+        assertEquals(9,response.getBody().size());
+
     }
 
-    @Test
-    void getByTheaterIdAndHallName() {
-    }
+    //UNFINISHED.. WIERD ERROR
 
     @Test
     void getByDateAndTheaterId() {
+        HttpEntity<String> entity = new HttpEntity<>(null,headers);
+        LocalDate date = LocalDate.parse("2025-11-12");
+        int id = ids[0].get(1);
+        String url = makeUrl(BASE_PATH+"/theater/{id}/getByDate");
+        Map<String, Integer> urlParams = new HashMap<>();
+        urlParams.put("id", id);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
+                .queryParam("date", date);
+
+        System.out.println(builder.buildAndExpand(urlParams).toUri());
+
+        ResponseEntity<List<ProjectionDTO>> response = restTemplate.exchange(builder
+                .buildAndExpand(urlParams).toUri(),HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<ProjectionDTO>>(){});
+
+        assertEquals("Dark Knight", response.getBody().get(0).getMovie().getName());
     }
+
 
     @Test
     void getProjectionsBetweenDates() {
     }
 
-    @Test
-    void testGetAll() {
+    private String makeUrl(String path){
+        String pathBuilt = "http://localhost:"+port+path;
+        System.out.println(pathBuilt);
+        return pathBuilt;
     }
 
-    @Test
-    void testGetById() {
+    private ResponseEntity<List<ProjectionDTO>> getResponseFromAllProjections() {
+        HttpEntity<String> entity = new HttpEntity<>(null,headers);
+        ResponseEntity<List<ProjectionDTO>> response = restTemplate.exchange(makeUrl(BASE_PATH),
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<ProjectionDTO>>() {});
+        return response;
     }
 
-    @Test
-    void testGetByTheaterId() {
-    }
-
-    @Test
-    void getByHallId() {
-    }
-
-    @Test
-    void testGetByTheaterIdAndHallName() {
-    }
-
-    @Test
-    void testGetByDateAndTheaterId() {
-    }
-
-    @Test
-    void testGetProjectionsBetweenDates() {
-    }
 }
