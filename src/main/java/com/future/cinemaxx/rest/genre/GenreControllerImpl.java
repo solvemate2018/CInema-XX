8e7e74e47b04fba45e853bf18fb8cee3d1cf2890
petr1940 <petr1940@stud.kea.dk>
2021-10-14 17:17:50 +0200

    genre endpoint

---
+++ src/main/java/com/future/cinemaxx/rest/genre/GenreControllerImpl.java
@@ -0,0 +1,50 @@
+package com.future.cinemaxx.rest.genre;
+
+import com.future.cinemaxx.dtos.GenreDTO;
+import com.future.cinemaxx.dtos.converter.DTOConverter;
+import com.future.cinemaxx.services.genres.GenreServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/api/genre")
+public class GenreControllerImpl implements GenreControllerInterface {
+
+    @Autowired
+    GenreServiceInterface genreService;
+    @Autowired
+    DTOConverter dtoConverter;
+
+    public GenreControllerImpl(GenreServiceInterface genreService, DTOConverter dtoConverter){
+        this.genreService = genreService;
+        this.dtoConverter = dtoConverter;
+    }
+
+    @Override
+    public List<GenreDTO> getAll() {
+        return genreService.getAllGenres().stream().map(genre -> dtoConverter.convertToGenreDTO(genre)).collect(Collectors.toList());
+    }
+
+    @Override
+    public GenreDTO getById(int id) {
+        return dtoConverter.convertToGenreDTO(genreService.getGenreById(id));
+    }
+
+    @Override
+    public GenreDTO create(GenreDTO genreDTO) {
+        return dtoConverter.convertToGenreDTO(genreService.createGenre(dtoConverter.convertToGenre(genreDTO)));
+
+    }
+
+
+    @Override
+    public void delete(int id) {
+        genreService.deleteGenre(id);
+
+    }
+}
