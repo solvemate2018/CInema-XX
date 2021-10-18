802f5c7bfc0261578f8ea45ccf3ff38275e368c7
Dell <bartek007k@gmail.com>
2021-10-18 17:53:25 +0200

    Added a rest endpoint for booking a ticket.

--- src/main/java/com/future/cinemaxx/repositories/TicketRepo.java
+++ src/main/java/com/future/cinemaxx/repositories/TicketRepo.java
@@ -4,6 +4,11 @@ import com.future.cinemaxx.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TicketRepo extends JpaRepository<Ticket, Long> {
+    void deleteByProjection_Id(int id);
+    Ticket getTicketByProjection_IdAndTicketRowAndTicketColumn(int projectionId, int rows, int column);
+    List<Ticket> getTicketsByProjection_id(int projectionId);
 }
---
+++ src/main/java/com/future/cinemaxx/rest/ticket/TicketControllerImpl.java
@@ -0,0 +1,42 @@
+package com.future.cinemaxx.rest.ticket;
+
+import com.future.cinemaxx.dtos.TicketDTO;
+import com.future.cinemaxx.dtos.converter.DTOConverter;
+import com.future.cinemaxx.services.tickets.TicketServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/ticket")
+public class TicketControllerImpl implements TicketControllerInterface{
+
+    TicketServiceInterface ticketService;
+    DTOConverter dtoConverter;
+
+    @Autowired
+    public TicketControllerImpl(TicketServiceInterface ticketService, DTOConverter dtoConverter){
+        this.ticketService = ticketService;
+        this.dtoConverter = dtoConverter;
+    }
+    @Override
+    public TicketDTO bookTicket(int projectionId, int row, int column) {
+        return dtoConverter.convertToTicketDTO(ticketService.bookTicket(projectionId,row,column));
+    }
+    public List<TicketDTO> getAll(){
+        return ticketService.getAllTickets().stream().map(ticket -> dtoConverter.convertToTicketDTO(ticket)).collect(Collectors.toList());
+    }
+
+    @Override
+    public TicketDTO getTicketByProjectionRowAndColumn(int projectionId, int row, int column) {
+        return dtoConverter.convertToTicketDTO(ticketService.getTicketByProjectionRowColumn(projectionId,row,column));
+    }
+
+    @Override
+    public List<TicketDTO> getTicketsByProjectionId(int projectionId) {
+        return ticketService.getTicketByProjection(projectionId).stream().map(ticket -> dtoConverter.convertToTicketDTO(ticket)).collect(Collectors.toList());
+    }
+}
